be654c5ef3429393f2dafb506e7f224038ab1f0d
korolvs <korolvs@gmail.com>
2016-07-26 00:12:51 +0300

    Mobile layout

---
+++ frontend/app/modules/common/js/directives/leftPanelDirective.js
@@ -0,0 +1,26 @@
+(function () {
+  'use strict';
+  
+  angular
+    .module('thatsaboy.common')
+    .directive('leftPanel', leftPanel);
+
+  leftPanel.$inject = ['$timeout'];
+
+  function leftPanel($timeout) {
+    return {
+      restrict: 'E',
+      replace: true,
+      transclude: true,
+      templateUrl: '/app/modules/common/templates/left-panel.html',
+      controller: function ($scope) {
+        $scope.panelClosed = true;
+        $scope.togglePanel = function(){
+          $timeout(function () {
+            $scope.panelClosed = !$scope.panelClosed;
+          }, 10);
+        };
+      }
+    };
+  }
+})();
