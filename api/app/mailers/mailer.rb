0b10ca2343af2b38b1c17b74fe4f837699d5c926
korolvs <korolvs@gmail.com>
2016-08-07 17:41:24 +0300

    Fixes #21

--- api/app/mailers/mailer.rb
+++ api/app/mailers/mailer.rb
@@ -1,13 +1,13 @@
 # Mailer
 class Mailer < ActionMailer::Base
-  default from: 'notifications@example.com'
+  default from: 'info@thatsaboy.com'
 
   # Sends email to confirm email
   # @param [String] email
   # @param [String] code
   def confirmation_email(email, code)
     @url = ENV['SITE_CONFIRM_LINK'] + '/' + code
-    mail(to: email, subject: 'Thanks for registration in That\'s a boy!')
+    mail(to: email, subject: 'Thanks for registration at Thatsaboy!')
   end
 
   # Sends email to recovery password
@@ -15,6 +15,6 @@ class Mailer < ActionMailer::Base
   # @param [String] code
   def recovery_email(email, code)
     @url = ENV['SITE_RECOVERY_LINK'] + '/' + code
-    mail(to: email, subject: 'Password recovery request in That\'s a boy!')
+    mail(to: email, subject: 'Password recovery request at Thatsaboy')
   end
 end
