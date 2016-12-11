ff310f7219cda11ef6d3b95859d70a9d3207ef6c
korolvs <korolvs@gmail.com>
2016-12-11 17:46:56 +0100

    Updated db schema file

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -22,12 +21,11 @@ ActiveRecord::Schema.define(version: 20160202185421) do
     t.integer  "goal_id"
     t.integer  "adult_id"
     t.datetime "created_at"
+    t.index ["adult_id"], name: "index_actions_on_adult_id", using: :btree
+    t.index ["goal_id"], name: "index_actions_on_goal_id", using: :btree
+    t.index ["id"], name: "index_actions_on_id", using: :btree
+    t.index ["user_id"], name: "index_actions_on_user_id", using: :btree
   end
-
-  add_index "actions", ["adult_id"], name: "index_actions_on_adult_id", using: :btree
-  add_index "actions", ["goal_id"], name: "index_actions_on_goal_id", using: :btree
-  add_index "actions", ["id"], name: "index_actions_on_id", using: :btree
-  add_index "actions", ["user_id"], name: "index_actions_on_user_id", using: :btree
 
   create_table "adults", force: :cascade do |t|
     t.string   "name",       default: ""
@@ -36,10 +34,9 @@ ActiveRecord::Schema.define(version: 20160202185421) do
     t.datetime "deleted_at"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.index ["id"], name: "index_adults_on_id", using: :btree
+    t.index ["user_id"], name: "index_adults_on_user_id", using: :btree
   end
-
-  add_index "adults", ["id"], name: "index_adults_on_id", using: :btree
-  add_index "adults", ["user_id"], name: "index_adults_on_user_id", using: :btree
 
   create_table "children", force: :cascade do |t|
     t.string   "name",       default: ""
@@ -48,10 +45,9 @@ ActiveRecord::Schema.define(version: 20160202185421) do
     t.datetime "deleted_at"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.index ["id"], name: "index_children_on_id", using: :btree
+    t.index ["user_id"], name: "index_children_on_user_id", using: :btree
   end
-
-  add_index "children", ["id"], name: "index_children_on_id", using: :btree
-  add_index "children", ["user_id"], name: "index_children_on_user_id", using: :btree
 
   create_table "families", force: :cascade do |t|
     t.string   "name",       default: ""
@@ -59,10 +55,9 @@ ActiveRecord::Schema.define(version: 20160202185421) do
     t.integer  "user_id"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.index ["id"], name: "index_families_on_id", using: :btree
+    t.index ["user_id"], name: "index_families_on_user_id", using: :btree
   end
-
-  add_index "families", ["id"], name: "index_families_on_id", using: :btree
-  add_index "families", ["user_id"], name: "index_families_on_user_id", using: :btree
 
   create_table "goals", force: :cascade do |t|
     t.string   "name",       default: ""
@@ -74,11 +69,10 @@ ActiveRecord::Schema.define(version: 20160202185421) do
     t.datetime "deleted_at"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.index ["child_id"], name: "index_goals_on_child_id", using: :btree
+    t.index ["id"], name: "index_goals_on_id", using: :btree
+    t.index ["user_id"], name: "index_goals_on_user_id", using: :btree
   end
-
-  add_index "goals", ["child_id"], name: "index_goals_on_child_id", using: :btree
-  add_index "goals", ["id"], name: "index_goals_on_id", using: :btree
-  add_index "goals", ["user_id"], name: "index_goals_on_user_id", using: :btree
 
   create_table "photos", force: :cascade do |t|
     t.string   "file_file_name"
@@ -89,10 +83,9 @@ ActiveRecord::Schema.define(version: 20160202185421) do
     t.datetime "deleted_at"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.index ["id"], name: "index_photos_on_id", using: :btree
+    t.index ["user_id"], name: "index_photos_on_user_id", using: :btree
   end
-
-  add_index "photos", ["id"], name: "index_photos_on_id", using: :btree
-  add_index "photos", ["user_id"], name: "index_photos_on_user_id", using: :btree
 
   create_table "tokens", force: :cascade do |t|
     t.string   "code",       default: ""
@@ -101,10 +94,9 @@ ActiveRecord::Schema.define(version: 20160202185421) do
     t.integer  "user_id"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.index ["id"], name: "index_tokens_on_id", using: :btree
+    t.index ["user_id"], name: "index_tokens_on_user_id", using: :btree
   end
-
-  add_index "tokens", ["id"], name: "index_tokens_on_id", using: :btree
-  add_index "tokens", ["user_id"], name: "index_tokens_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",              default: ""
@@ -114,10 +106,9 @@ ActiveRecord::Schema.define(version: 20160202185421) do
     t.string   "pin",                default: "0000"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", using: :btree
+    t.index ["id"], name: "index_users_on_id", using: :btree
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", using: :btree
-  add_index "users", ["id"], name: "index_users_on_id", using: :btree
 
   add_foreign_key "actions", "adults"
   add_foreign_key "actions", "goals"
