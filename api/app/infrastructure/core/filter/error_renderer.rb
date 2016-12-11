0d337f1a1bf47bda4ae18230bf06eeafef59a2d2
korolvs <korolvs@gmail.com>
2016-12-11 21:44:24 +0100

    Refactored error renderer

--- api/app/infrastructure/core/filter/error_renderer.rb
+++ api/app/infrastructure/core/filter/error_renderer.rb
@@ -8,19 +8,19 @@ class Core::Filter::ErrorRenderer
       @controller = controller
       yield
     rescue Core::Errors::BadRequest => e
-      self.render 400, JSON.generate(:error => e.message || 'Bad Request')
+      render 400, JSON.generate(error: e.message || 'Bad Request')
     rescue Core::Errors::UnauthorizedError => e
-      self.render 401, JSON.generate(:error => e.message || 'Unauthorized')
+      render 401, JSON.generate(error: e.message || 'Unauthorized')
     rescue Core::Errors::ForbiddenError => e
-      self.render 403, JSON.generate(:error => e.message || 'Forbidden')
+      render 403, JSON.generate(error: e.message || 'Forbidden')
     rescue Core::Errors::NotFoundError => e
-      self.render 404, JSON.generate(:error => e.message || 'Not Found')
+      render 404, JSON.generate(error: e.message || 'Not Found')
     rescue ActiveRecord::RecordNotFound => e
-      self.render 404, JSON.generate(:error => e.message || 'Not Found')
+      render 404, JSON.generate(error: e.message || 'Not Found')
     rescue ActiveRecord::RecordInvalid => e
-      self.render 422, e.record.errors.to_json
+      render 422, e.record.errors.to_json
     rescue StandardError => e
-      self.render 500, JSON.generate({ error: e.message, class: e.class, backtrace: e.backtrace })
+      render 500, JSON.generate({ error: e.message, class: e.class, backtrace: e.backtrace })
     end
 
     # Handle errors and process them
